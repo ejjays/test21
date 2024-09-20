59ced54a21a02be1ec770b4ef6400e731de825ab
ejjays <christsonalloso021@gmail.com>
2024-09-20 19:33:59 +0800

    Update settingpannel.js

--- settingpannel.js
+++ settingpannel.js
@@ -15,24 +15,7 @@ backButton.addEventListener('click', () => {
     settingsPanel.classList.remove('open');
 });
 
-// Toggle mic icon
-micIcon.addEventListener('click', () => {
-    micIcon.classList.toggle('active'); // Toggles the active class
-    if (micIcon.classList.contains('active')) {
-        micIcon.classList.remove('fa-microphone'); // Change to muted icon
-        micIcon.classList.add('fa-microphone-slash');
-        // Optionally mute the audio track
-        const audioTracks = localStream.getAudioTracks();
-        audioTracks.forEach(track => track.enabled = false);
-    } else {
-        micIcon.classList.remove('fa-microphone-slash'); // Change back to unmuted icon
-        micIcon.classList.add('fa-microphone');
-        // Optionally unmute the audio track
-        const audioTracks = localStream.getAudioTracks();
-        audioTracks.forEach(track => track.enabled = true);
-    }
-});
- 
+
 // Toggle video icon
 videoIcon.addEventListener('click', () => {
     videoIcon.classList.toggle('active'); // Toggles the active class
