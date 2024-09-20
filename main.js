aa3cc2eed404f036d018fb8f572186425ca338b0
ejjays <christsonalloso021@gmail.com>
2024-09-21 00:37:47 +0800

    Update main.js

--- main.js
+++ main.js
@@ -40,6 +40,11 @@ const hangupButton = document.getElementById('hangupButton');
 const videoIcon = document.getElementById('video-icon');
 const micIcon = document.getElementById('mic-icon');
 
+// Side panel elements
+const settingsIcon = document.getElementById('settings-icon');
+const settingsPanel = document.getElementById('settings-panel');
+const backButton = document.getElementById('back-button');
+
 let isVideoOn = false;
 
 function adjustVideoSizes() {
@@ -56,6 +61,15 @@ function adjustVideoSizes() {
         remoteVideoBox.classList.add('remote-size');
     }
 }
+
+// Toggle settings panel
+settingsIcon.addEventListener('click', () => {
+    settingsPanel.classList.toggle('open');
+});
+
+backButton.addEventListener('click', () => {
+    settingsPanel.classList.remove('open');
+});
 
 videoIcon.onclick = async () => {
     const localVideo = document.getElementById('webcamVideo');
